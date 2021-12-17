decf67c66681b3a6f4f2af13bc4648b82722ec09
Allison Demkovich <demkovich.allison@gmail.com>
2021-12-16 18:01:47 -0600

    add window prompts for password criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,19 @@
 // Assignment code here
 
+// ask the user what password criteria they have
+var confirmNumber = window.confirm("Would you like your password to be LONGER than 8 characters?");
+var confirmSpecialChar = window.confirm("Would you like your password to contain special characters?");
+
+// if yes, they do want more than 8 characters in their password
+if (confirmNumber) {
+  console.log("yes, more than 8 characters");
+}
+
+// if yes, they do want special characters in their password
+if (confirmSpecialChar) {
+  console.log("yes, special characters");
+}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
